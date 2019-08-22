09ec7077facdd6c8df8bbd6f3171c5c9374520e7
adymoloca <ady.moloca@yahoo.com>
2019-08-23 02:45:10 +0300

    Updates

---
+++ precache-manifest.88bc95a32bc5557e42bd799e4a0b41e4.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2c86eedd5f51f7c964bc9b9ea4afe727",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "8bda4cbc21670e10bc52",
+    "url": "/monsters-rolodex/static/css/main.b635b742.chunk.css"
+  },
+  {
+    "revision": "d81979aa8ac68af22713",
+    "url": "/monsters-rolodex/static/js/2.4ea1ec15.chunk.js"
+  },
+  {
+    "revision": "8bda4cbc21670e10bc52",
+    "url": "/monsters-rolodex/static/js/main.be03bbcf.chunk.js"
+  },
+  {
+    "revision": "c7f991e6cb4746d6bd30",
+    "url": "/monsters-rolodex/static/js/runtime~main.205b164d.js"
+  }
+]);
